b0fe254861bc302ddea43044f1d26dcd3265b8db
Lukas <l.seiler@im-en.com>
2018-12-30 22:15:36 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+use std::net::UdpSocket;
+use std::env;
+use std::str;
+use std::thread;
+use std::time::{Duration, Instant};
+
+struct Config {
+    shore_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let shore_name = args[1].clone();
+
+        Config{shore_name}        
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    let my_shore_point = ShorePoint{name: config.shore_name, ip: None};
+    let lighthouse_thread = thread::spawn(move || {run_lighthouse(&my_shore_point);});
+    let shore_points = run_crows_nest();
+    println!("Found shore_points: {:?}", shore_points.len());
+    lighthouse_thread.join().unwrap();
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ShorePoint{
+    name: String,
+    ip: Option<String>,
+}
+
+fn run_crows_nest() -> Vec<ShorePoint>
+{
+    let socket = UdpSocket::bind("127.0.0.1:6667").expect("could not bind to address");
+    socket.set_broadcast(true).expect("set_broadcast call failed");
+    let time_out_duration = Duration::new(10,0);
+    let option : Option<Duration> = Option::from(time_out_duration);
+    socket.set_read_timeout(option).expect("set_read_timeout call failed");
+    // socket.set_nonblocking(true).expect("could not set non-blocking");
+    let whos_there = "abcd";
+    socket.send_to(whos_there.as_bytes(), "255.255.255.255:6666").expect("could not send broadcast");
+    
+    let start = Instant::now();
+    let mut buf = [0u8; 1024];
+    let mut shore_points: Vec<ShorePoint> = Vec::new();
+    while start.elapsed().as_secs() < 5 {
+        let (amt, src) = match socket.recv_from(&mut buf)
+        {
+            Ok(r) => r,
+            Err(e) => {
+                println!("{}",e);
+                continue;
+            },
+            
+        };
+        let filled_buffer = &buf[..amt];
+        let payload = str::from_utf8(&filled_buffer).expect("Error converting payload to string");
+
+        // Convert the JSON string back to a Point.
+        let deserialized: ShorePoint = match serde_json::from_str(&payload)
+        {
+            Ok(r) => r,
+              Err(e) => {
+                println!("{}",e);
+                continue;
+            }, 
+        };                                                                 
+        println!("{:?}", deserialized);
+        shore_points.push(deserialized);
+        thread::sleep(Duration::from_millis(50));
+        
+    }
+
+    return shore_points
+
+}
+
+fn add_to_shore_sightings(shorepoint :ShorePoint)
+{
+
+}
+
+fn run_lighthouse(shore_point: &ShorePoint){
+    // let socket = create_and_bind_socket();
+    let socket = UdpSocket::bind("127.0.0.1:6666").expect("could not bind to address");
+    let mut buf = [0; 1024];
+    loop {
+        
+        let (amt, src) = match socket.recv_from(&mut buf)
+        {
+            Ok(r) => r,
+            Err(e) => {
+                println!("{}",e);
+                return ();
+            },
+            
+        };
+        let filled_buffer = &buf[..amt];
+        let code_word = str::from_utf8(&filled_buffer).unwrap();
+        println!("{}: {:?}", src, code_word);
+        if code_word == "abcd" {
+            println!("{}",shore_point.name);                                                   
+            let serialized_shore = serde_json::to_string(shore_point).expect("Error serializing Shore Point");
+            socket.send_to(serialized_shore.as_bytes(), src).expect("Error sending Shore Point");
+        }
+        
+    }
+   
+}
+
+// fn create_and_bind_socket(){
+// {
+    // let socket = UdpSocket::bind("127.0.0.1:6666").expect("could not bind to address!");
+// }
